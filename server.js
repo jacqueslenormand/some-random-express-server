788293e363772c976fe458bb7385814e01f615b3
jacques <jacqueslenormand@gmail.com>
2018-10-29 20:20:58 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+let express = require('express')
+let app = express()
+
+app.get('/', function(req, res) {
+    res.send("<h2> this is express!</h2>")
+})
+
+app.listen(3030, function() {
+    "I started!"
+})
