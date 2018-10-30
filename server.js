e882542c485db7aa4222898efd761107cc2bac04
jacques <jacqueslenormand@gmail.com>
2018-10-29 20:29:38 -0400

    newer version

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express')
 let app = express()
 
 app.get('/', function(req, res) {
-    res.send("<h2> this is express!</h2>")
+    res.send("<h2> this is express Version 2!</h2>")
 })
 
 app.listen(3030, function() {
